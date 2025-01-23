ca25f4b92194da6b644e19dde32be50a3c3400c3
Andrii Tusha <Andrii_Tusha@epam.com>
2025-01-23 18:08:05 +0200

    Migrate to Supabase

--- server/db.js
+++ server/db.js
@@ -5,13 +5,20 @@ dotenv.config();
 console.log("DB_PASSWORD:", process.env.DB_PASSWORD);
 
 const sequelize = new Sequelize(
-    process.env.DB_NAME,
-    process.env.DB_USER,
-    process.env.DB_PASSWORD,
+    // process.env.DB_NAME,
+    // process.env.DB_USER,
+    // process.env.DB_PASSWORD,
+    process.env.NEXT_PUBLIC_SUPABASE_URL,
     {
         dialect: "postgres",
-        host: process.env.DB_HOST,
-        port: process.env.DB_PORT,
+        // host: process.env.DB_HOST,
+        // port: process.env.DB_PORT,
+        dialectOptions: {
+            ssl: {
+                require: true,
+                rejectUnauthorized: false
+            }
+        }
     }
 )
 
